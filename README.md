4f2bac23885aa690faa361e72cc7ba41a729eaa7
xunull <todogo@vip.163.com>
2018-04-17 16:24:23 +0800

    add readme introduce

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # speedtest
+
+测试各种语言运算速度，运算使用加法 每次加一
+
+node 代码中引入的模块在npm root -g 目录下，如果报错Cannot find module，需要设置NODE_PATH环境变量
+
--- runFunc.py
+++ runFunc.py
@@ -1,6 +1,8 @@
 import os
 
+
 def runTest(cmd, language):
+    os.chdir('./codes')
     result = os.popen(cmd).read()
     print(language)
     print(result)
